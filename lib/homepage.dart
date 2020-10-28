ef8ab59762ebe4357f5f2b7be84be6fee038ce8f
Abir Ahsan <abir@Abirs-MacBook-Pro.local>
2020-10-29 02:36:42 +0600

    add appbar with search icon

---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("All TV"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("Clicked on Search Icon");
+            },
+          ),
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
